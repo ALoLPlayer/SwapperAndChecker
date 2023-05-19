624c438fefcc89f98568e8465437892c038a1440
jv <116314546+ALoLPlayer@users.noreply.github.com>
2023-05-19 17:23:08 +0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,62 @@
-package com.example.swapperandchecker;
+package com.example.swapper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText text1, text2;
+    private Button btnSwap, btnCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text2);
+        btnSwap = findViewById(R.id.btnSwap);
+        btnCheck = findViewById(R.id.btnCheck);
 
+        btnSwap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swapText();
+            }
+        });
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkText();
+            }
+        });
+
+    }
+
+    private void swapText() {
+        String temp = text1.getText().toString();
+        text1.setText(text2.getText().toString());
+        text2.setText(temp);
+    }
+
+    private void checkText() {
+        String str1 = text1.getText().toString();
+        String str2 = text2.getText().toString();
+
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+
+        if (str1.equals(str2)) {
+            intent.putExtra("result", "SAME");
+            startActivity(intent);
+        } else {
+            intent.putExtra("result", "NOT THE SAME");
+            startActivity(intent);
+        }
     }
 }
---
+++ ResultActivity.java
@@ -0,0 +1,24 @@
+package com.example.swapper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        resultTextView = findViewById(R.id.resultTextView);
+
+        Intent intent = getIntent();
+        String result = intent.getStringExtra("result").toString();
+        resultTextView.setText(result);
+    }
+}
